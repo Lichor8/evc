b5cc39aefc8cd667ce55f3558a911dd13f18b3a9
Rik <r.j.m.v.d.struijk@student.tue.nl>
2016-05-09 11:55:22 +0200

    working ROI on canny image

--- src/RoadDetection_rik3.py
+++ src/RoadDetection_rik3.py
@@ -24,25 +24,11 @@ roiSize = Point(0, 0)
 roiGap = Point(0, 0)
 
 # import test image
-imgP = cv2.imread('../roadtests/artiroad0.png')
-img = cv2.imread('../roadtests/artiroad0.png')
-
-# region of interest (roi) directly in front of vehicle
-imgShape = img.shape
-imgSize.x = imgShape[1]     # save the size of the image in pixels
-imgSize.y = imgShape[0]
-roiSize.x = 50              # define size of roi window in percentage of total image size
-roiSize.y = 50
-roiGap.x = int(0.5*(imgSize.x - roiSize.x*imgSize.x/100))   # gap calculated as even spaces from image borders
-roiGap.y = int(imgSize.y - roiSize.y*imgSize.y/100)         # gap calculated as from the top of the image
-
-roi = img[roiGap.y:imgSize.y, roiGap.x:imgSize.x - roiGap.x]        # set roi
-imgRoi = np.zeros((imgSize.y, imgSize.x, 3), np.uint8)              # create new RGB black image
-imgRoi[:imgSize.y, :imgSize.x] = (255, 255, 255)                    # turn image white
-imgRoi[roiGap.y:imgSize.y, roiGap.x:imgSize.x - roiGap.x] = roi     # add roi to white image
+imgP = cv2.imread('../roadtests/artiroad1.jpg')
+img = cv2.imread('../roadtests/artiroad1.jpg')
 
 # blur image using a gaussian blur
-imgBlurred = cv2.GaussianBlur(imgRoi, (5, 5), 0)
+imgBlurred = cv2.GaussianBlur(img, (5, 5), 0)
 
 # define range of grey color in RGB
 # lower_gray = np.array([160, 160, 160])
@@ -54,26 +40,39 @@ imgBlurred = cv2.GaussianBlur(imgRoi, (5, 5), 0)
 # detect edges in image using canny
 imgCanny = cv2.Canny(imgBlurred, 50, 150, apertureSize=3)
 
+# region of interest (roi) directly in front of vehicle
+imgShape = img.shape
+imgSize.x = imgShape[1]     # save the size of the image in pixels
+imgSize.y = imgShape[0]
+roiSize.x = 50              # define size of roi window in percentage of total image size
+roiSize.y = 50
+roiGap.x = int(0.5*(imgSize.x - roiSize.x*imgSize.x/100))   # gap calculated as even spaces from image borders
+roiGap.y = int(imgSize.y - roiSize.y*imgSize.y/100)         # gap calculated as from the top of the image
+
+roi = imgCanny[roiGap.y:imgSize.y, roiGap.x:imgSize.x - roiGap.x]       # get roi from image
+imgRoi = np.zeros((imgSize.y, imgSize.x), np.uint8)                 # create new binary black image
+imgRoi[roiGap.y:imgSize.y, roiGap.x:imgSize.x - roiGap.x] = roi     # add roi to black image
+
 # show all steps as images
-cv2.imshow('ROI', roi)
 # cv2.imshow('grayscaled', imgGrayscale)
 cv2.imshow('imgblurred', imgBlurred)
 cv2.imshow('imgcanny', imgCanny)
+cv2.imshow('ROI', roi)
 
 # set heuristic parameters for the (probabilistic) houghlines function
 rho = 1                 # accuracy [pixel]
 acc = 1                 # accuracy [deg]
 theta = acc*np.pi/360
 thresholdP = 10         # minimum points on a line
-threshold = 80          # minimum points on a line
+threshold = 50          # minimum points on a line
 minLineLength = 2      # line segments shorter than this are rejected
 maxLineGap = 10         # maximum allowed gap between line segments to treat them as single line
 
 # find lines in image using the (probabilistic) houghlines function
 # outputP:  x1, y1, x2, y2
 # output:   rho, theta
-linesP = cv2.HoughLinesP(imgCanny, rho, theta, thresholdP, minLineLength, maxLineGap)
-lines = cv2.HoughLines(imgCanny, rho, theta, threshold)
+linesP = cv2.HoughLinesP(imgRoi, rho, theta, thresholdP, minLineLength, maxLineGap)
+lines = cv2.HoughLines(imgRoi, rho, theta, threshold)
 
 # check if houghlinesP function found any lines, if not print error
 if linesP is not None:
