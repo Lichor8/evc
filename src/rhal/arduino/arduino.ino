98da0e9f0ee96146df3f775f83ff73fe196d84b5
Rik <r.j.m.v.d.struijk@student.tue.nl>
2016-06-10 18:52:32 +0200

    testing motor controller, only kinematic now

--- src/rhal/arduino/arduino.ino
+++ src/rhal/arduino/arduino.ino
@@ -52,6 +52,7 @@ void loop()
   
   // outer loop control
   float e_old = 0.0;
+  float E = 0.0;
   float omega = 0.0;
   float v     = 0.0;
   
@@ -87,8 +88,8 @@ void loop()
     //}
     //delay(500);
     
-    x_d = 10;    // [m]
-    y_d = 10;    // [m]  
+    x_d = 0;    // [m]
+    y_d = 1;    // [m]  
     
     // location2angle
     //phi_a = pi;   
@@ -97,10 +98,10 @@ void loop()
     //phi_d = 0.0;
     
     phi_d = atan2(y_d - y_a, x_d - x_a);
-    
+        
     // outer loop control (pcontrol_freq)
-    pcontrol(phi_d, phi_a, omega, v, e_old);  
-    //Serial.println(omega);
+    pcontrol(phi_d, phi_a, omega, v, e_old, E);  
+    Serial.println(omega);
     
     // inverse kinematics
     const float R = 0.07;         // [m]
@@ -108,8 +109,8 @@ void loop()
     float theta_dot_d_l = 0.0;  
     float theta_dot_d_r = 0.0;    
     
-    theta_dot_d_r = (2*v + omega*L)/(2*R);
-    theta_dot_d_l = (2*v - omega*L)/(2*R);
+    theta_dot_d_r = (2*v + omega*L)/(2*R);  // [rad/s]
+    theta_dot_d_l = (2*v - omega*L)/(2*R);  // [rad/s]
     
     // sensor sample loop (sensor_freq)
     int MotorL = 0;
@@ -172,31 +173,31 @@ void loop()
           float motor_encoder_ertime_l = millis(); 
           
           // calculate differences
-          float dt_l = motor_encoder_ertime_l - motor_encoder_srtime_l_previous; //[ms]
+          float dt_l = (motor_encoder_ertime_l - motor_encoder_srtime_l_previous)/1000; //[s]
           float dp_l = pulse_count_l - pulse_count_l_old;
           
-          // pulse2mm
-          float ds_l = dp_l*(0.6);    // [mm]
+          // pulse2rad (12 magnets in sensor, gear ratio 34:1)
+          float ds_l = dp_l*2*pi/(12*34);   // [rad]
           
           // calculate speed
-          theta_dot_a_l = ds_l/dt_l;  // m/s
+          theta_dot_a_l = ds_l/dt_l;        // [rad/s]
           
           //// right motor 
           // end read time of encoder       
           float motor_encoder_ertime_r = millis();  
           
           // calculate differences
-          float dt_r = (motor_encoder_ertime_r - motor_encoder_srtime_r_previous); // [ms]
+          float dt_r = (motor_encoder_ertime_r - motor_encoder_srtime_r_previous)/1000; // [s]
           float dp_r = pulse_count_r - pulse_count_r_old;
           
-          // pulse2rad
-          float ds_r = dp_r*(0.6);    // [mm]
+          // pulse2rad (12 magnets in sensor, gear ratio 34:1)
+          float ds_r = dp_r*2*pi/(12*34);    // [rad]
           
           // calculate speed
-          theta_dot_a_r = ds_r/dt_r;  // [m/s]
+          theta_dot_a_r = ds_r/dt_r;  // [rad/s]
               
           // static controller gains
-          const int K[2] = {1, 1};
+          const int K[2] = {0.3101, -0.0688};
           const int F = 0;
               
           //Serial.println(j*5);
@@ -222,13 +223,15 @@ void loop()
           float r_l = theta_dot_d_l;
           float x1_l = theta_dot_a_l;
           float x2_l = dc_current_a_l;
-          float c_l = K[0]*x1_l + K[1]*x2_l + F*r_l;
+          //float c_l = K[0]*x1_l + K[1]*x2_l + F*r_l;
+          float c_l = (theta_dot_d_l - theta_dot_a_l)*1;
           MotorL = 255*c_l;
           
           float r_r = theta_dot_d_r;
           float x1_r = theta_dot_a_r;
           float x2_r = dc_current_a_r;
-          float c_r = K[0]*x1_r + K[1]*x2_r + F*r_r;
+          //float c_r = K[0]*x1_r + K[1]*x2_r + F*r_r;
+          float c_r = (theta_dot_d_r - theta_dot_a_r)*1;
           MotorR = 255*c_r;
           
           //Serial.println(c_l);
@@ -281,17 +284,17 @@ void setMotor(const unsigned char cucPWM, const unsigned char cucFWD , const uns
   analogWrite(cucPWM, abs(ciSpeed));  
 }
 
-void pcontrol(float phi_d, float phi_a, float &omega, float &v, float &e_old)
+void pcontrol(float phi_d, float phi_a, float &omega, float &v, float &e_old, float &E)
 {  
   const int Kp = 1;
   const int Ki = 1;
-  const int Kd = 1;
-  float E = 0.0; 
+  const int Kd = 1; 
   
   float e = phi_d - phi_a;
   float e_dot = e - e_old;
   E = E + e;
-  omega = Kp*e + Ki*E + Kd*e_dot;
+  //omega = Kp*e + Ki*E + Kd*e_dot;
+  omega = Kp*e;
   e_old = e;
   
   v = 0.25;            // [m/s]
