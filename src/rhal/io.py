97190bb5bdc44c2aad4fe9ff7b0783593036912e
Rik <r.j.m.v.d.struijk@student.tue.nl>
2016-08-15 13:13:22 +0200

    working on communication integration and added tutorials rpi_connections + run_python_scripts

---
+++ src/rhal/io.py
@@ -0,0 +1,13 @@
+import serial
+import time
+
+
+def initialize():
+    # In testing the Python script, it seems the problem is that the Arduino resets when you open the serial port
+    # (at least the Uno does), so you need to wait a few seconds for it to start up.
+
+    # initialize serial in main? and send ser? (Serial is also in currentsensor!)
+    ser = serial.Serial('/dev/ttyUSB0', 9600)
+    time.sleep(2)  # wait for Arduino
+
+    return ser
