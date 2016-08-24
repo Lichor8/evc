7efb955df9c3f08014010dfa88021fd9d848293e
Rik <r.j.m.v.d.struijk@student.tue.nl>
2016-08-24 16:53:54 +0200

    testing string compare

--- src/main.py
+++ src/main.py
@@ -2,6 +2,19 @@
 import rhal.rhal
 import detection.detection
 
+# compare strings with numbers
+import re
+
+def tryint(x):
+    try:
+        return int(x)
+    except ValueError:
+        return x
+
+
+def splittedname(s):
+    return tuple(tryint(x) for x in re.split('([0-9]+)', s))
+
 # create robot objects from classes
 rhal = rhal.rhal.Rhal()
 det = detection.detection.Detection()
@@ -24,14 +37,25 @@ while io_ok:
         sdata = '2d1|'          # mov_type = 2 (drive),                 d (distance) in [m]
         sdata = '3r0.2|'        # mov_type = 3 (turn right),            r (turn radius) in [m]
         sdata = '4a180|'        # mov_type = 4 (turn),                  a (angle) in [deg]
-        sdata = '5t6|'          # mov_type = 5 (stop),                  t (time) in [s]
+        sdata = '5t1|'          # mov_type = 5 (stop),                  t (time) in [s]
 
         rhal.setsdata(sdata)
 
         # trigger coordinator/composer of rhal
         rhal.rhal()
         rdata = rhal.get_rdata()
-        print(rdata)
+        # print(rdata)
+
+        rdata_split = splittedname(rdata)
+        # print(rdata_split)
+        test = "('done', 5)"
+        print(test)
+
+        if rdata_split == test:
+            print(rdata)
+
+        if set(rdata.split()) == set('done5'):
+            print(rdata)
 
         # trigger coordinator/composer of detection
         # det.detection()
